de59bf792cb84fd8f521c455853cce78458e284f
Oleksandr Pavlyk <sashapvk7@gmail.com>
2026-06-07 12:28:23 +0000

    documentazione

--- README.md
+++ README.md
@@ -1,2 +1,128 @@
-# ADS1
-Risolutore dei numeri di Stirling di seconda specie e dei numeri di Bell
+# ADS1 - DOCUMENTAZIONE TECNICA
+
+ADS1 è uno dei modelli dell’Algoritmo di Stirling, il cui compito è determinare il numero totale di partizioni di un insieme.
+
+Un insieme è una collezione definita di oggetti, detti elementi, riferibili a entità astratte o reali. In un insieme non sono ammessi duplicati.
+Un insieme può contenere sottoinsiemi, cioè sotto-collezioni di elementi appartenenti all’insieme originale.
+
+Una partizione è una raccolta di sottoinsiemi, detti blocchi, che coprono l'insieme originale senza sovrapposizioni e senza elementi ripetuti tra i blocchi.
+Il numero totale di partizioni di un insieme di n elementi è dato dal numero di Bell B_n
+Il numero di modi per partizionare un insieme di n elementi in esattamente k blocchi è dato dal numero di Stirling di seconda specie.
+
+ADS1 utilizza metodi computazionali basati su formule ricorsive per calcolare i numeri di Stirling e, di conseguenza, i numeri di Bell in modo analitico e deterministico.
+Nel corso dell’anno scolastico 2025/2026 ho implementato ADS1 nei linguaggi C e Python.
+Il programma, oltre a includere il nucleo computazionale, integra anche un’interfaccia CLI che consente l’interazione con l’utente e semplifica l’assegnazione degli insiemi da analizzare.
+## DESCRIZIONE ALGORITMICA
+
+Il programma è composto dalle seguenti procedure:
+- INIT: inizializza la sintassi del'input, ovvero gli operatori con cui l'utente comunica l'insieme da elaborare
+- ASGN: assegnazione/acquizione e elaborazione/interpretazione della stringa, inserita dall’utente, che rappresenta l'insieme 
+- CRD: calcolo della cardinalità dell’insieme
+- TDS: il calcolo dei numeri di Stirling di seconda specie, utilizzati per determinare il numero di Bell
+- BLL: restituzione del numero di Bell
+- ESC: procedura tecnica di deallocazione della memoria (nel caso del C) e gestione della terminazione del programma o dell’avvio di una nuova computazione
+Dopo INIT, il programma continua l’esecuzione a meno che l’utente non interrompa il flusso durante ASGN o ESC.
+
+Ogni fase usa funzioni di supporto per compiti specifici. In particolare:
+- `leggi_char`: legge un singolo carattere dall'utente in modo sicuro.
+- `gia_usato`: verifica se un carattere è stato già inserito durante la personalizzazione degli operatori, da poter evitare casi come OPS = [=, =, =, =]
+- `controllare`: controlla la presenza dei tre operatori principali(corresponsore, terminatore aperto, terminatore chiuso) e il loro ordine
+- `estrarre`: funzione di parsing ovvero di tokenizzazione manuale, con eliminazione degli spazi all'inizio e alla fine della sottostringa estratta
+
+## INTERFACCIA CLI
+
+La CLI guida l'utente durante l'esecuzione del programma.
+Le funzionalita principali sono:
+- configurare gli operatori;
+- leggere l'insieme da elaborare;
+- validare l'input;
+- mostrare i risultati;
+- decidere se eseguire un'altra computazione.
+
+La sintassi adottata da ADS1 per la rappresentazione degli insiemi è composta da quattro operatori:
+- corresponsore: separa il denominatore(nome dell'insieme) dal contenuto dell'insieme
+- terminatore aperto: identifica l'inizio dell'elenco degli elementi
+- terminatore chiuso: identifica la fine dell'elenco degli elementi
+- separatore: distingue un elemento dal successivo
+
+Nella configurazione predefinita tali operatori corrispondono rispettivamente ai simboli:
+=  {  }  ,
+e consentono di rappresentare un insieme nella forma:
+    A = {a1, a2, ... aN}
+dove A rappresenta il denominatore dell'insieme e a1, a2 .. aN rappresentano i suoi elementi.
+ADS1 consente all'utente di personalizzare ciascun operatore durante la fase di inizializzazione, mantenendo invariata la struttura logica dell'input
+    Esempio: A > [a1; a2; ... aN]
+
+Durante l'esecuzione, ADS1 utilizza un sistema di conferma basato sulle risposte s/n (sì/no) per le operazioni che richiedono una scelta da parte dell'utente:
+- accettazione degli operatori predefiniti
+- avvio di una nuova computazione
+- terminazione del programma
+In caso di risposta non valida, il sistema richiede nuovamente l'inserimento senza interrompere l'esecuzione.
+Gli input non conformi vengono rifiutati e non vengono inoltrati alle procedure di elaborazione.
+Per consentire l'interruzione immediata dell'applicazione, ADS1 riconosce inoltre il comando 'exit' digitabile durante la fase di acquisizione dell'insieme.
+
+Dopo la fase di acquisizione, il programma mostra l'insieme normalizzato senza duplicati e con la sintassi corretta.
+Vengono inoltre visualizzate:
+- cardinalità effettiva dell'insieme
+- degenerazione dell'insieme (numero di duplicati rimossi)
+- numero totale delle partizioni possibili
+
+Prima dell'esecuzione del nucleo computazionale, l'utente può scegliere se stampare il triangolo di Stirling generato durante il calcolo
+
+## NUCLEO COMPUTAZIONALE
+
+L'elemento centrale di ADS1 responsabile della costruzione del triangolo di Stirling e della determinazione del numero di Bell.
+Dal punto di vista matematico, l'algoritmo implementa la relazione ricorsiva dei numeri di Stirling di seconda specie:
+$$S(n, k) = k * S(n-1, k) + S(n-1, k-1)$$
+
+Nel codice vengono usati tecniche di shifting e autoaggiornamento, anziché la ricorsione vera.
+Si usa un solo array dinamico `T` di dimensione `N+1`.
+L'array viene aggiornato in ordine decrescente di `k` per mantenere i valori precedenti.
+
+L'array inizia con `T[0] = 1`.
+Per ogni `n` da 1 a `N`, il programma calcola:
+``T[k] = (k + 1) * T[k] + T[k - 1]``
+per `k` che va da `n` a 1.
+
+Alla fine, il numero di Bell è la somma di tutti i valori dell'array T nell'ultima configurazione.
+
+## PRESTAZIONI DEL CODICE
+
+### Prestazioni numeriche (complessita)
+
+Il calcolo principale usa operazioni semplici:
+- moltiplicazioni;
+- addizioni;
+- assegnazioni.
+
+Il tipo usato per i risultati e `long long`.
+TDS fa crescere il tempo di esecuzione piu o meno come `N^2`.
+
+### Prestazioni operative
+
+Il programma segue questo flusso:
+INIT -> ASGN -> CRD -> TDS -> BLL -> ESC
+
+Ogni procedura ha un lavoro preciso e passa il risultato alla fase successiva.
+L'esecuzione è sequenziale e non usa parallelismo.
+
+### Prestazioni tecniche
+
+#### Tempo di esecuzione
+
+Le procedure piu esigenti sono:
+- CRD, che confronta gli elementi per eliminare i duplicati;
+- TDS, che aggiorna l'array per calcolare i numeri di Stirling.
+
+Nel caso peggiore, entrambe le procedure crescono in modo quadratico.
+
+##### Uso della memoria
+
+La memoria cresce in modo lineare con il numero di elementi.
+Il programma usa un solo array dinamico `T` di lunghezza `N+1`, quindi non costruisce una matrice completa.
+
+##### Uso della CPU
+
+Il codice è single-threaded.
+Non ci sono costi di sincronizzazione.
+La CPU lavora di piu quando l'insieme contiene piu elementi.
--- main.c
+++ main.c
@@ -1,31 +1,32 @@
-#include <stdio.h> // Per input/output
-#include <string.h> // Per funzioni di manipolazione delle stringhe come strcmp e strchr
-#include <stdlib.h> // Per funzioni di allocazione e deallocazione della memoria
+// ADS1.c - modello analitico deterministico con formula di Stirling
 
-//operatori default
+#include <stdio.h>
+#include <string.h> 
+#include <stdlib.h> 
+// ho usato array dinamici perché MSVC non supporta i VLA (es. int A[N])
+
+// Operatori che servono ad interpretare la stringa di input, con possibilità di personalizzazione da parte dell'utente
 char OPS[4] = {'=', '{', '}', ','};
-// Stringhe descrittive per gli operatori, utilizzate nei messaggi all'utente
+// Stringhe utilizzate nei messaggi all'utente
 char *OPSnomi[4] = {"corresponsore", "terminatore aperto", "terminatore chiuso", "separatore"};
-char *OPSes[4] = {"'=', '>'", 
-    "'{', '(', '['",
-    "'}', ')', ']'",
+char *OPSes[4] = {"'=' '>'", 
+    "'{' '(' '['",
+    "'}' ')' ']'",
     "',' ';'"
 };
 
-// Funzione per leggere un carattere, ignorando eventuali spazi o newline
 char leggi_char() {
     char c = getchar();
     while (c == '\n') c = getchar();
     while (getchar() != '\n');
     return c;
 }
-// Funzione per verificare se un carattere è stato già inserito durante la personalizzazione degli operatori
+// Verifica se un carattere è stato già inserito durante la personalizzazione degli operatori, da poter evitare casi come OPS = [=, =, =, =]
 int gia_usato(char OPS[], int candidato) {
     for (int i = 0; i < candidato; i++)  if (OPS[i] == OPS[candidato]) return 1;
     return 0;
 }
-// Funzione per controllare se la stringa di input rispetta il formato richiesto (denominatore, terminatore aperto, terminatore chiuso)
-// Quindi verifica sia la presenza dei tre operatori principali che il loro ordine corretto nella stringa
+// Controllo della presenza dei tre operatori principali(corresponsore, terminatore aperto, terminatore chiuso) e del loro ordine
 int controllare(char *stringa) {
     char *cr = strchr(stringa, OPS[0]);
     char *ta = strchr(stringa, OPS[1]);
@@ -33,85 +34,79 @@ int controllare(char *stringa) {
     if ((!cr || !ta || !tc) || (!(cr < ta && ta < tc))) return 0;
     return 1;
 }
-// Funzione per estrarre una sottostringa delimitata da un carattere terminatore specifico, aggiornando il puntatore di posizione nella stringa originale
-char *estrarre(char *stringa, int *puntatore, char termine) {
+// Tokenizzatore manuale, con eliminazione degli spazi all'inizio e alla fine della sottostringa estratta
+char *estrarre(char *stringa, int *puntatore, char target) {
     if (!stringa || !puntatore) return NULL;
     int i = *puntatore;
     while (stringa[i] == ' ') i++;
     int start = i;
-    while (stringa[i] != '\0' && stringa[i] != termine) i++;
+    while (stringa[i] != '\0' && stringa[i] != target) i++;
     int fine = i;
     while (fine > start && stringa[fine - 1] == ' ') fine--;
     int lun = fine - start;
-    char *pezzo = malloc(lun + 1);
-    if (!pezzo) return NULL;
-    for (int j = 0; j < lun; j++) pezzo[j] = stringa[start + j];
-    pezzo[lun] = '\0';
-    if (stringa[i] == termine) i++;
+    char *sottostringa = malloc(lun + 1);
+    if (!sottostringa) return NULL;
+    for (int j = 0; j < lun; j++) sottostringa[j] = stringa[start + j];
+    sottostringa[lun] = '\0';
+    if (stringa[i] == target) i++;
     *puntatore = i;
-    return pezzo;
-}
-// NUCLEO COMPUTAZIONALE
-// Funzione che sfrutta i numeri di Stirling di seconda specie nel modo non convenzionale per generare il 'triangolo di Stirling'
-// e calcolare il numero di Bell, ovvero il numero di tutti partizioni possibili di un insieme di N elementi
-// con la minima complessità computazionale e di memoria possibile, con output del triangolo se richiesto dall'utente
-long long bell_stirling(long long N, long long **T, int pr) {
-    for (int n = 0; n < N; n++) {
-        T[n][0] = 1;
-        for (int k = n; k >= 1; k--) {
-            T[n][k] = (k + 1) * T[n][k] + T[n][k - 1];
-        }
-        for (int k = 0; k <= n; k++) {
-            if (pr) printf("%lld ", T[n][k]);
-            if (n + 1 < N) T[n + 1][k] = T[n][k];
-        }
-        if (pr) printf("\n");
-    }
-    long long B = 0;
-    for (int k = 0; k < N; k++) B += T[N-1][k];
-    return B;
+    return sottostringa;
 }
 
 int main() {
     // INIT - stabilizzazione degli operatori
-    printf("Benvenuto su ADS1!\n");
+    printf("\nBenvenuto su ADS1!\n");
+    printf("Per inizializzare un insieme segui lo standard => A %c %ca1%c a2%c ... aN%c \n", 
+            OPS[0], OPS[1], OPS[3], OPS[3], OPS[2]);
+    printf("Gli elementi non devono coincidere con gli operatori.\n");
+    printf("I duplicati saranno ignorati.\n");
+    int el_len = 1;
+    while (1){
+        printf("Digita 1 per utilizzare come gli elementi i singoli simboli alfanumerici\n");
+        printf("Digita 0 per utilizzare come gli elementi le stringhe alfanumeriche di lunghezza arbitraria.\n");
+        printf("Scelta: ");
+        char risposta = leggi_char();
+        if (risposta != '0' && risposta != '1'){
+            printf("Risposta non valida\n");
+        } else {
+            el_len = (risposta == '1') ? 1 : 0;
+            break;
+        }
+    }
     while (1) {
-        printf("Operatori default: "
+        printf("\nOperatori default: "
             "\n\t%c (%s) \n\t%c (%s) \n\t%c (%s) \n\t%c (%s)\n", 
             OPS[0], OPSnomi[0], OPS[1], OPSnomi[1], OPS[2], OPSnomi[2], OPS[3], OPSnomi[3]);
-        printf("Vuoi procedere con operatori default? (s/n): ");
+        printf("Vuoi procedere con gli operatori di default? (s/n): ");
         char risposta = leggi_char();
         if (risposta == 's' || risposta == 'S') {
-            printf("Operatori default accettati.\n");
+            printf("Operatori di default accettati.\n");
             break;
         } else if (risposta == 'n' || risposta == 'N') {
+            printf("\n");
             for (int o = 0; o<4; o++) {
                 OPS[o] = '\0';
-                printf("Stabilire %s (es. %s): ", OPSnomi[o], OPSes[o]);
-                OPS[o] = leggi_char();
-                while (gia_usato(OPS, o)) {
-                    printf("Carattere gia usato, inserire un altro carattere: ");
+                while(1){
+                    printf("Stabilire %s (es. %s): ", OPSnomi[o], OPSes[o]);
                     OPS[o] = leggi_char();
+                    if (gia_usato(OPS, o)) {
+                        OPS[o] = '\0';
+                        printf("Carattere gia inserito previamente\n");
+                    } else { break;}
                 }
             }
-            printf("Operatori personalizzati accettati."
+            printf("\nOperatori personalizzati accettati."
                 "\n\t%c (%s) \n\t%c (%s) \n\t%c (%s) \n\t%c (%s)\n", 
             OPS[0], OPSnomi[0], OPS[1], OPSnomi[1], OPS[2], OPSnomi[2], OPS[3], OPSnomi[3]);
+            printf("Nuovo standard: A %c %ca1%c a2%c ... aN%c \n", OPS[0], OPS[1], OPS[3], OPS[3], OPS[2]);
             break;
         } else  printf("Risposta non valida\n");
     }
     while(1){
-        // ASGN - prodecura principale di input dell'utente
-        char *temp = NULL;
-        char *stringa = NULL;
-        int capacita = 0;
-        int lunghezza = 0;
+        // ASGN - elaborazione della stringa di input, estrazione degli elementi dell'insieme
+        char *temp = NULL; char *stringa = NULL; int capacita = 0; int lunghezza = 0;
         int ch = 0;
-        printf("\nPer uscire, digita 'exit'\n");
-        printf("Per inizializzare insieme segui lo standard => A %c %ca%c b%c c%c \n", 
-            OPS[0], OPS[1], OPS[3], OPS[3], OPS[2]);
-        printf("Gli elementi possono essere stringhe di caratteri alfanumerici, ma non possono contenere gli operatori.\n");
-        printf("Per separare gli elementi usa '%c'\n", OPS[3]);
+        printf("\nPer uscire, digita 'exit'\nAvviso importante: massima cardinalita consigliata = 25\n");
         printf("\nPrompt: ");
         int rinvio = 0;
         while ((ch = getchar()) != '\n' && ch != EOF) {
@@ -120,7 +115,7 @@ int main() {
                 temp = realloc(stringa, capacita * sizeof(char));
                 if (temp == NULL) {
                     free(stringa);
-                    printf("Errore: memoria insufficiente.\n");
+                    printf("Errore di allocazione della memoria.\n");
                     rinvio = 1;
                     break;
                 }
@@ -130,9 +125,9 @@ int main() {
         }
         if (rinvio) continue;
         if (stringa) stringa[lunghezza] = '\0';
-        if (stringa && strcmp(stringa, "exit") == 0) {
+        if (stringa && !strcmp(stringa, "exit")) {
             free(stringa);
-            printf("Arrivederci!\n");
+            printf("Arrivederci!\n"); 
             break;
         }
         if (stringa == NULL || controllare(stringa) == 0) {
@@ -140,39 +135,67 @@ int main() {
             free(stringa);
             continue;
         }
-        // CRD - procedura di elaborazione della stringa di input, estrazione del denominatore e degli elementi dell'insieme, 
-        // rimozione dei duplicati e calcolo della cardinalità e degenerazione
         printf("\nElaborazione della richiesta....\n");
-        int separati = 0;
+        int separati = 0; // serve per contare numero di separatori, da poter allocare uno spazio per gli elementi inseriti dall'utente
         for (int i = 0; i < lunghezza; i++) if (stringa[i] == OPS[3]) separati++;
-        int puntatore = 0;
-        char *denominatore = estrarre(stringa, &puntatore, OPS[0]);
-        if (denominatore == NULL) { printf("Errore di formattazione del denominatore.\n"); continue;}
+        int puntatore = 0; //serve per scanning manuale della stringa
+        char *denominatore = estrarre(stringa, &puntatore, OPS[0]); // nome dell'insieme
+        if (denominatore == NULL) {
+            free(stringa);
+            printf("Errore di elaborazione del denominatore.\n"); 
+            continue;
+        }
         while (stringa[puntatore] != OPS[1] && stringa[puntatore] != '\0') puntatore++;
         if(stringa[puntatore] == OPS[1]) puntatore++;
         int puntatore_backup = puntatore;
         char *contenuto = estrarre(stringa, &puntatore, OPS[2]);
-        if (contenuto == NULL) { printf("Errore di elaborazione della stringa"); continue;}
+        if (contenuto == NULL) {
+            free(denominatore);
+            free(stringa); 
+            printf("Errore di elaborazione della stringa"); 
+            continue;
+        }
         if(strlen(contenuto)==0){
-            printf("Insieme vuoto\nCardinalita: 0 \nDegenerazione: 0\nIl numero di tutti partizioni possibili: 1\n");
+            printf("Insieme vuoto\nCardinalita: 0 \nDegenerazione: 0\nIl numero di tutte le partizioni possibili: 1\n");
             free(stringa); free(denominatore); free(contenuto);
+            int nuova = 0;
             while (1) {
                 printf("\nVuoi eseguire un'altra computazione?(s/n): ");
                 char risposta = leggi_char();
-                if (risposta == 's' || risposta == 'S') break;
-                else if (risposta == 'n' || risposta == 'N') { printf("Arrivederci!\n"); return 0;} 
+                if (risposta == 's' || risposta == 'S') {nuova = 1; break;}
+                else if (risposta == 'n' || risposta == 'N') { printf("Arrivederci!\n"); return 0;}
                 else printf("Risposta non valida\n");
             }
+            if (nuova) continue;
         } else separati += 1;
         free(contenuto);
         puntatore = puntatore_backup;
+        // CRD - calcolo della cardinalità e degenerazione
         char **insieme = (char **)calloc((separati+1), sizeof(char *));
+        if (insieme == NULL) {
+            free(denominatore);
+            free(stringa);
+            printf("Errore di allocazione di memoria");
+            continue;
+        }
         int N = 0;
         int degenerazione = 0;
+        rinvio = 0;
         for (int e = 0; e < separati; e++) {
             char *elemento = estrarre(stringa, &puntatore, (e == separati - 1) ? OPS[2] : OPS[3]);
-            if (elemento == NULL) { printf("Errore di formattazione degli elementi"); continue;}
-            if (strlen(elemento) == 0) { free(elemento); continue;}
+            if (elemento == NULL) {
+                free(denominatore);
+                for(int f = 0; f < separati+1; f++) free(insieme[f]);
+                free(insieme);
+                free(stringa);
+                printf("Errore di formattazione degli elementi");
+                rinvio = 1;
+                break;
+            }
+            if (el_len ? (strlen(elemento) != 1) : (strlen(elemento) == 0)) {
+                free(elemento);
+                continue;
+            }
             int duplicato = 0;
             for (int i = 0; i < N; i++) {
                 if (strcmp(insieme[i], elemento) == 0) {
@@ -183,7 +206,9 @@ int main() {
             if (!duplicato) insieme[N++] = elemento;
             else { free(elemento); degenerazione++; }
         }
+        if (rinvio) continue;
         free(stringa);
+        // output dell'insieme normalizzato (senza duplicati e seguendo la sintassi standard)
         printf("%s %c %c", denominatore, OPS[0], OPS[1]);
         for (int i = 0; i < N; i++) {
             printf("%s", insieme[i]);
@@ -201,21 +226,39 @@ int main() {
             } else if (risposta == 'n' || risposta == 'N') break;
             else printf("Risposta non valida\n");
         }
-        // TDS + BLL - computazione del numero di Bell tramite triangolo di Stirling
+        // TDS - NUCLEO COMPUTAZIONALE
+        // calcolo del triangolo di Stirling(uso un solo array e applico tecniche di shifting e autoaggiornamento mostrate in classe)
         printf("\nComputazione in corso...\n");
-        long long **T = (long long **)malloc(N * sizeof(long long *));
-        if (T == NULL) return 1;
-        for (int i = 0; i < N; i++) T[i] = (long long *)calloc(N, sizeof(long long));
-        long long B = bell_stirling(N, T, stampare_TDS);
-        printf("\nIl numero di tutti partizioni possibili: %lld\n", B);
-        // FIN - procedura di pulizia della memoria e richiesta di nuova computazione
-        for (int f = 0; f < N; f++) {free(insieme[f]); free(T[f]);}
-        free(insieme); free(T); free(denominatore);
+        long long *T = (long long *)calloc(N + 1, sizeof(long long));
+        if (T == NULL) {
+            printf("Errore di inizializzazione"); 
+            free(denominatore);
+            for(int f = 0; f < N; f++) free(insieme[f]);
+            free(insieme); 
+            continue;
+        }
+        T[0] = 1;
+        for (int n = 1; n <= N; n++) {
+            if (stampare_TDS) { 
+                for (int k = 0; k < n; k++) printf("%lld ", T[k]);
+                printf("\n");
+            }
+            if (n!=N) {
+                for (int k = n; k >= 1; k--) T[k] = (k+1) * T[k] + T[k - 1];
+            }
+        }
+        // BLL - calcolo del numero di Bell, che è obbiettivo finale
+        long long B = 0;
+        for (int k = 0; k < N; k++) B += T[k];
+        printf("\nIl numero di tutte le partizioni possibili: %lld\n", B);
+        // ESC - pulizia della memoria. scelta tra nuova computazione e uscita
+        for (int f = 0; f < N; f++) {free(insieme[f]);}
+        free(insieme); free(denominatore); free(T);
         while (1) {
             printf("\nVuoi eseguire un'altra computazione?(s/n): ");
             char risposta = leggi_char();
             if (risposta == 's' || risposta == 'S') break;
-            else if (risposta == 'n' || risposta == 'N') { printf("Arrivederci!\n"); return 0;} 
+            else if (risposta == 'n' || risposta == 'N') { printf("Arrivederci!\n"); return 0;}
             else printf("Risposta non valida\n");
         }
     }
